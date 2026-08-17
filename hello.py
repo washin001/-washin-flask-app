f4191839d6d1198166d0e6573d85f2407f086b2b
Washington Do Nascimento Sousa <wnnnnnd@gmail.com>
2026-08-17 02:48:47 +0000

    Semana 05 - Formularios web

--- hello.py
+++ hello.py
@@ -1,10 +1,61 @@
-# A very simple Flask Hello World app for you to get started with...
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return '<p>Alterações por meio do PythonAnyWhere -> GitHub</p><table><tr><td><b>Professor:</b></td><td>Professor Fabio Teixeira</td></tr><tr><td><b>Prontuário:</b></td><td>PT23820X</td></tr></table>'
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'Washington_PT303755X_Aula050'
+
+
+class NameForm(FlaskForm):
+    name = StringField(
+        'What is your name?',
+        validators=[DataRequired()]
+    )
+
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    form = NameForm()
+
+    if form.validate_on_submit():
+
+        old_name = session.get('name')
+
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+
+        session['name'] = form.name.data
+
+        return redirect(url_for('index'))
+
+    return render_template(
+        'index.html',
+        form=form,
+        name=session.get('name')
+    )
+
+
+@app.route('/identificacao')
+def identificacao():
+
+    return render_template(
+        'identificacao.html',
+        nome='Washington',
+        prontuario='PT303755X',
+        professor='Fabio Teixeira'
+    )
+
+
+@app.route('/contextorequisicao')
+def contexto_requisicao():
+
+    return render_template('contexto.html')
+
+
+if __name__ == '__main__':
+    app.run()
