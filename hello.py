6e1c873413a63e87af8f00ca9cb79b6067487317
Washington Do Nascimento Sousa <washington.sousa@aluno.ifsp.edu.brgit config --global user.name Washington>
2026-09-07 00:43:24 +0000

    Semana 08 - bancos de dados

--- hello.py
+++ hello.py
@@ -1,105 +1,151 @@
-from flask import Flask, render_template, request, session
-from datetime import datetime
+import os
+from flask import Flask, render_template, session, redirect, url_for
+
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'chave-secreta-washington'
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
-def tempo_decorrido(delta):
-    """Retorna um texto tipo 'an hour', '5 minutes', 'a day' a partir de um timedelta."""
-    segundos = int(delta.total_seconds())
+class Role(db.Model):
+    __tablename__ = 'roles'
 
-    if segundos < 60:
-        return "a few seconds"
+    id = db.Column(db.Integer, primary_key=True)
 
-    minutos = segundos // 60
-    if minutos == 1:
-        return "a minute"
-    if minutos < 60:
-        return f"{minutos} minutes"
+    name = db.Column(db.String(64), unique=True)
 
-    horas = minutos // 60
-    if horas == 1:
-        return "an hour"
-    if horas < 24:
-        return f"{horas} hours"
+    users = db.relationship(
+        'User',
+        backref='role',
+        lazy='dynamic'
+    )
 
-    dias = horas // 24
-    if dias == 1:
-        return "a day"
-    return f"{dias} days"
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    username = db.Column(
+        db.String(64),
+        unique=True,
+        index=True
+    )
+
+    role_id = db.Column(
+        db.Integer,
+        db.ForeignKey('roles.id')
+    )
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class NameForm(FlaskForm):
+
+    name = StringField(
+        'What is your name?',
+        validators=[DataRequired()]
+    )
+
+    submit = SubmitField('Submit')
+
+
+@app.shell_context_processor
+def make_shell_context():
+
+    return dict(
+        db=db,
+        User=User,
+        Role=Role
+    )
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+
+    return render_template('500.html'), 500
 
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
+def index():
 
-    nome = None
-    sobrenome = None
-    instituicao = None
-    disciplina = None
+    form = NameForm()
 
-    if request.method == 'POST':
-        nome = request.form.get('nome')
-        sobrenome = request.form.get('sobrenome')
-        instituicao = request.form.get('instituicao')
-        disciplina = request.form.get('disciplina')
+    if form.validate_on_submit():
 
-    agora = datetime.now()
-    horario = agora.strftime('%B %d, %Y %-I:%M %p')
+        user = User.query.filter_by(
+            username=form.name.data
+        ).first()
 
-    # Guarda/lê o horário da última visita na sessão do usuário
-    ultima_visita = session.get('ultima_visita')
-    session['ultima_visita'] = agora.isoformat()
+        if user is None:
 
-    mensagem_tempo = ''
-    if ultima_visita:
-        delta = agora - datetime.fromisoformat(ultima_visita)
-        mensagem_tempo = tempo_decorrido(delta)
+            user_role = Role.query.filter_by(
+                name='User'
+            ).first()
+
+            if user_role is None:
+
+                user_role = Role(name='User')
+
+                db.session.add(user_role)
+
+                db.session.commit()
+
+            user = User(
+                username=form.name.data,
+                role=user_role
+            )
+
+            db.session.add(user)
+
+            db.session.commit()
+
+            session['known'] = False
+
+        else:
+
+            session['known'] = True
+
+        session['name'] = form.name.data
+
+        return redirect(url_for('index'))
+
+    users = User.query.order_by(User.id).all()
 
     return render_template(
         'index.html',
-        nome=nome,
-        sobrenome=sobrenome,
-        instituicao=instituicao,
-        disciplina=disciplina,
-        ip=request.remote_addr,
-        host=request.host,
-        horario=horario,
-        mensagem_tempo=mensagem_tempo
+        form=form,
+        name=session.get('name'),
+        known=session.get('known', False),
+        users=users
     )
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-
-    mensagem = ''
-
-    if request.method == 'POST':
-
-        usuario = request.form.get('usuario')
-        senha = request.form.get('senha')
-
-        if usuario == 'admin' and senha == '1234':
-            mensagem = 'Login realizado com sucesso!'
-        else:
-            mensagem = 'Usuário ou senha inválidos.'
-
-    return render_template(
-        'login.html',
-        mensagem=mensagem
-    )
-
-
-@app.route('/identificacao')
-def identificacao():
-    return render_template('identificacao.html')
-
-
-@app.route('/contexto')
-def contexto():
-    return render_template('contexto.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
